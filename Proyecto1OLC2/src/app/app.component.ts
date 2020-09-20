b4e1bad69655b88eea7cff7d635ddc707e7ccdc8
201800726 <57029729+201800726@users.noreply.github.com>
2020-09-20 08:29:27 -0600

    :art:

--- Proyecto1OLC2/src/app/app.component.ts
+++ Proyecto1OLC2/src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Proyecto1OLC2';
+  content ="";
+  obj;
+  obj2;
+
+  ngOnInit(){
+    this.obj= "//Ejemplo funciones anidadas\n"+
+    "\n"+
+    "//funcion abuelo\n"+
+    "function abuelo(a: number, b: number) {\n"+
+    "\n"+
+    "    //funcion padre\n"+
+    "    function padre(a: string, b: boolean) {\n"+
+    "    \n"+
+    "        //funcion anidada\n"+
+    "        function nieto(a: number, b: number) {\n"+
+    "            return a * b;\n"+
+    "        }\n"+
+    "        \n"+
+    "        return b ? \"Mi abuelo es \" + a : \"Mi abuelo no es \" + a;\n"+
+    "    }\n"+
+    "    return a + b;\n"+
+    "}\n"+
+    "\n"+
+    "var sum: number = addNumbers(10,15)\n"+
+    "console.log('Sum of the two numbers is: '+sum)\n";
+
+    this.obj2="//funcion abuelo\n"+
+    "function abuelo(a: number, b: number) {\n"+
+    "    return a + b;\n"+
+    "}\n"+
+    "\n"+
+    "//funcion anidada\n"+
+    "function abuelo_padre(a: string, b: boolean) {\n"+
+    "    return b ? \"Mi abuelo es \" + a : \"Mi abuelo no es \" + a;\n"+
+    "}\n"+
+    "\n"+
+    "//funcion anidada\n"+
+    "function abuelo_padre_nieto(a: number, b: number) {\n"+
+    "    return a * b;\n"+
+    "}\n"+
+    "\n"+
+    "var sum: number = addNumbers(10, 15)\n"+
+    "console.log('Sum of the two numbers is: ' + sum)\n";
+  }
+  setEditorContent(event) {
+    // console.log(event, typeof event);
+    console.log(this.obj);
+  }
+  setEditor2Content(event) {
+    // console.log(event, typeof event);
+    console.log(this.obj2);
+  }
 }
--- Proyecto1OLC2/src/app/app.module.ts
+++ Proyecto1OLC2/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CodemirrorModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
